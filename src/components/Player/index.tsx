ed478a857f83605a86f37fa813428bf20cbcb617
acidbutter96 <marknotas@outlook.com>
2021-04-21 00:28:42 -0300

    style: set app style

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss';
+
+export default function Player() {
+
+    return (
+        <div className={styles.playerContainer}>
+            <img src="/logo.svg" alt="Podcastr" />
+            <span>
+
+            </span>
+        </div>
+    )
+}
