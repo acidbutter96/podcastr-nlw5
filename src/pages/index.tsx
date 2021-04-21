ec80543a97d48ef14edaf1bc373b29c2f1349b9b
acidbutter96 <marknotas@outlook.com>
2021-04-21 02:12:04 -0300

    refactor: server json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,38 @@
-export default function Home() {
+import { useEffect } from 'react';
+
+export default function Home(props) {
+
   return (
     <>
-
+      <h1>Index</h1>
     </>
   )
 }
 
+export async function getStaticProps() {
+
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 28800,
+  }
+}
+
+/* export async function getServerSideProps(){
+
+    const response = await fetch('http://localhost:3333/episodes')
+    const data = await response.json()
+
+    return {
+      props:{
+        episodes: data,
+
+      }
+    }
+}
+ */
 /* missaoespacial */
